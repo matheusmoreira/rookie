203d8af419684aaff3ab042190c09d2e71a9c9c0
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2011-12-19 07:30:01 -0200

    Fix Logger and improve Git task.

--- lib/rookie/tasks/git.rb
+++ lib/rookie/tasks/git.rb
@@ -1,4 +1,5 @@
 require 'git'
+require 'logger'
 require 'rake/tasklib'
 
 module Rookie
@@ -8,10 +9,14 @@ module Rookie
       attr_accessor :release_version
 
       def initialize(release_version = nil, working_dir = Dir.pwd)
-        self.git = ::Git.open working_dir, :log => Logger.new(STDOUT)
+        self.git = ::Git.open working_dir, :log => ::Logger.new(STDOUT)
         self.release_version = release_version
         yield self if block_given?
         define
+      end
+
+      def release_tag(version = release_version)
+        "v#{version.to_s}"
       end
 
       def tag!(tag_name)
@@ -23,19 +28,13 @@ module Rookie
         end
       end
 
-      def push!(remote = 'origin', branch = 'master')
-        git.push remote, branch
+      def push!(remote = 'origin', branch = 'master', tags = false)
+        git.push remote, branch, tags
       end
 
-      def push_tags!(remote = 'origin', branch = 'master')
-        git.push remote, branch, true
-      end
-
-      def release!(version = release_version, remote = 'origin', branch = 'master')
-        release_tag = "v#{version.to_s}"
-        tag! release_tag
-        push! remote, branch
-        push_tags! remote, branch
+      def release!(version_tag = release_tag, remote = 'origin', branch = 'master')
+        tag! version_tag
+        push! remote, branch, true
       end
 
       def already_tagged?(tag_name)
@@ -55,10 +54,12 @@ module Rookie
             push! args[:remote], args[:branch]
           end
 
-          desc 'Pushes tags to a remote repository'
-          task :push_tags, :remote, :branch do |task, args|
-            args.with_defaults remote: 'origin', branch: 'master'
-            push_tags! args[:remote], args[:branch]
+          namespace :push do
+            desc 'Pushes tags to a remote repository'
+            task :tags, :remote, :branch do |task, args|
+              args.with_defaults remote: 'origin', branch: 'master'
+              push! args[:remote], args[:branch]
+            end
           end
 
           desc 'Release current version'
