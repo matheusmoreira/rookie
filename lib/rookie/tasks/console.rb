24b282e9196377607ae1f0b0c9ac28089dae0134
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2011-12-30 09:49:06 -0200

    Add Rookie::Tasks::Console, an alternative to bundle console.

---
+++ lib/rookie/tasks/console.rb
@@ -0,0 +1,40 @@
+require 'rake/tasklib'
+
+module Rookie
+  class Tasks < ::Rake::TaskLib
+    class Console < ::Rake::TaskLib
+
+      attr_accessor :spec, :program
+      attr_writer :command
+
+      def command
+        @command ||= generate_command_string
+      end
+
+      def initialize(spec, opts = {})
+        self.spec = spec
+        self.program = opts.fetch :program, 'irb'
+        self.command = opts.fetch :command, nil
+        yield self if block_given?
+        define
+      end
+
+      def define
+        desc 'Starts an interactive ruby session with your gem loaded'
+        task :console do
+          sh command
+        end
+      end
+
+      def generate_command_string
+        program.dup.tap do |command_string|
+          spec.require_paths.each do |path|
+            command_string << ' -I ' << path
+          end
+          command_string << ' -r ' << spec.name
+        end
+      end
+
+    end
+  end
+end
