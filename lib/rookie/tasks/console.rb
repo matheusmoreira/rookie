7f41af84b728be3a9bc72e3128749583bbad5a01
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2012-01-20 05:15:15 -0200

    Document the Console task class.

--- lib/rookie/tasks/console.rb
+++ lib/rookie/tasks/console.rb
@@ -2,18 +2,38 @@ require 'rake/tasklib'
 
 module Rookie
   class Tasks < ::Rake::TaskLib
+
+    # This task provides an easy way to interactively test your gem by
+    # automatically loading and requiring it in an interactive ruby session.
     class Console < ::Rake::TaskLib
 
+      # The gem specification.
       attr_accessor :spec
 
+      # The name of the program to invoke.
       attr_accessor :program
 
+      # The full command to execute.
       attr_writer :command
 
+      # Unless set explicitly, will be automatically generated from the #program
+      # name and gem specification.
       def command
         @command ||= generate_command_string
       end
 
+      # Creates a new console task with the given parameters. Options may
+      # specify:
+      #
+      # [:program]  the name of the program to invoke; +irb+ by default.
+      # [:command]  the full command to execute. Use if the command you want to
+      #             run doesn't take the <tt>-I</tt> and <tt>-r</tt> command
+      #             line arguments.
+      #
+      # Yields the instance if given a block.
+      #
+      # Tasks do not get defined automatically; don't forget to call
+      # #define_tasks!
       def initialize(spec, opts = {})
         self.spec = spec
         self.program = opts.fetch :program, :irb
@@ -21,6 +41,7 @@ module Rookie
         yield self if block_given?
       end
 
+      # Defines the console task.
       def define_tasks!
         desc 'Starts an interactive ruby session with the gem loaded'
         task :console do
@@ -28,6 +49,10 @@ module Rookie
         end
       end
 
+      # Generates a command string from this task's #program name and gem
+      # specification. For example:
+      #
+      #   irb -I lib -r gem_name
       def generate_command_string
         program.to_s.dup.tap do |command_string|
           spec.require_paths.each do |path|
@@ -38,5 +63,6 @@ module Rookie
       end
 
     end
+
   end
 end
