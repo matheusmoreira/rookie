2f8795e6c91de9b2a361b97bc894b87a0f280022
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2012-06-04 20:32:11 -0300

    Add Rookie::root module method

--- lib/rookie.rb
+++ lib/rookie.rb
@@ -2,5 +2,14 @@
 module Rookie
 end
 
+class << Rookie
+
+  # Location of the Rookie gem.
+  def root
+    File.expand_path '..', File.dirname(__FILE__)
+  end
+
+end
+
 require 'rookie/tasks'
 require 'rookie/version'
